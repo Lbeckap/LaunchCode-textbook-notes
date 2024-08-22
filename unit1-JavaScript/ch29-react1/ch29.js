8c844b260642df827779e62af23ed1fd2cf41e4a
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-08-22 12:26:43 -0400

    updated notes

---
+++ unit1-JavaScript/ch29-react1/ch29.js
@@ -0,0 +1,7 @@
+// react 1
+
+// Why do developers choose React for there projects?
+
+//* library - a collection of reusable code, such as methods and classes, that developers can use to write more complex applications more efficiently.
+
+//* frameworks - are different from libraries. An example of a front-end JavaScript framework would be Angular. Frameworks control the flow of the application and provide structure for it. When working with frameworks, developers do not have to make any decisions related to flow and structure and are more focused on plugging in the appropriate code at the appropriate time.
---
+++ unit1-JavaScript/ch29-react1/react-part-one/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from './components/Hello'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Hello />
+      </div>
+    </>
+  )
+}
+
+export default App
